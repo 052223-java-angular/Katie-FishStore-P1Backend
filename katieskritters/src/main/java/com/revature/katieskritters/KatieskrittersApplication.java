92b01ac296faa15b7e9c0a2344e57264f5bee483
Chichior <katiehakala@icloud.com>
2023-06-02 14:19:26 -0700

    Initializing Maven-SpringBoot Project

---
+++ katieskritters/src/main/java/com/revature/katieskritters/KatieskrittersApplication.java
@@ -0,0 +1,13 @@
+package com.revature.katieskritters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatieskrittersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatieskrittersApplication.class, args);
+	}
+
+}
---
+++ katieskritters/src/test/java/com/revature/katieskritters/KatieskrittersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.katieskritters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatieskrittersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
