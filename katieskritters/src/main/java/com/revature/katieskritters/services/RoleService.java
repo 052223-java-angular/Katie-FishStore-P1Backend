b4a039e103b5c0094fbb3cae79f6b3d03b61ef76
Chichior <katiehakala@icloud.com>
2023-06-05 18:39:16 -0700

    Started register. Added BCrypt dependency.

--- katieskritters/src/main/java/com/revature/katieskritters/services/RoleService.java
+++ katieskritters/src/main/java/com/revature/katieskritters/services/RoleService.java
@@ -2,10 +2,16 @@ package com.revature.katieskritters.services;
 
 import org.springframework.stereotype.Service;
 
+import com.revature.katieskritters.entities.Role;
+
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class RoleService {
     private RoleService roleService;
+
+    public Role findByName(String string) {
+        return null;
+    }
 }
--- katieskritters/src/main/java/com/revature/katieskritters/services/UserService.java
+++ katieskritters/src/main/java/com/revature/katieskritters/services/UserService.java
@@ -2,8 +2,11 @@ package com.revature.katieskritters.services;
 
 import java.util.Optional;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import com.revature.katieskritters.dtos.requests.NewUserRequest;
+import com.revature.katieskritters.entities.Role;
 import com.revature.katieskritters.entities.User;
 import com.revature.katieskritters.repositories.UserRepository;
 
@@ -13,10 +16,33 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleService roleService;
+
+    public User register(String username, String password) {
+        Role role = roleService.findByName("USER");
+        String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+        User user = new User(username, hashed, role.getRole_id());
+        return userRepository.save(user);
+    }
 
     public boolean isUniqueUsername(String username) {
         Optional<User> userOpt = userRepository.findByUsername(username);
-        return userOpt.isEmpty();
+
+        if (userOpt.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 
+    public boolean isUsernameValid(String username) {
+        return username.matches("^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$");
+    }
+
+    public boolean isPasswordValid(String password) {
+        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+    }
+
+    public boolean isConfirmPasswordSame(String password, String confirmedPassword) {
+        return password.equals(confirmedPassword);
+    }
 }
